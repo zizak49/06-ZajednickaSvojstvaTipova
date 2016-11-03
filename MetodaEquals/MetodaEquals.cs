ddb091f88f046db335c0d89ba187811533706abe
Mislav Franjo <mislav.franja@gmail.com>
2016-11-03 21:00:13 +0100

    rijesili equals

--- MetodaEquals/MetodaEquals.cs
+++ MetodaEquals/MetodaEquals.cs
@@ -17,33 +17,35 @@ namespace Vsite.CSharp
             MojaKlasa mk1 = new MojaKlasa();
             MojaKlasa mk2 = null;
 
-            // TODO: Usporediti objekte mk1 i mk2 korištenjem statičke metode MojaKlasa.Equals te ispisati rezultat
+            // Usporediti objekte mk1 i mk2 korištenjem statičke metode MojaKlasa.Equals te ispisati rezultat
+            Console.WriteLine(MojaKlasa.Equals(mk1, mk2));
 
+            // Usporediti reference na objekte mk1 i mk2 korištenjem statičke metode MojaKlasa.ReferenceEquals te ispisati rezultat
+            Console.WriteLine(MojaKlasa.ReferenceEquals(mk1, mk2));
 
-            // TODO: Usporediti reference na objekte mk1 i mk2 korištenjem statičke metode MojaKlasa.ReferenceEquals te ispisati rezultat
+            // Usporediti objekte mk1 i mk2 pozivom metode mk1.Equals te ispisati rezultat
+            Console.WriteLine(mk1.Equals(mk2));
 
-
-            // TODO: Usporediti objekte mk1 i mk2 pozivom metode mk1.Equals te ispisati rezultat
-
-            
-            // TODO: Usporediti objekte mk1 i mk2 pozivom metode mk2.Equals te ispisati rezultat
-
+            // Usporediti objekte mk1 i mk2 pozivom metode mk2.Equals te ispisati rezultat
+            //Console.WriteLine(mk2.Equals(mk1));
 
         }
 
         public static void UsporedbaStruktura()
         {
-            // TODO: Storiti dvije strukture ms1 i ms2 na isti način kao i za gornje instance klasa mk1 i mk2
+            //  Storiti dvije strukture ms1 i ms2 na isti način kao i za gornje instance klasa mk1 i mk2
+            MojaStruktura ms1 = new MojaStruktura();
+            MojaStruktura ms2 = new MojaStruktura();
 
 
-            // TODO: Usporediti objekte ms1 i ms2 korištenjem statičke metode MojaStruktura.Equals te ispisati rezultat
+            // Usporediti objekte ms1 i ms2 korištenjem statičke metode MojaStruktura.Equals te ispisati rezultat
+            Console.WriteLine(MojaStruktura.Equals(ms1, ms2));
 
+            // Usporediti objekte ms1 i ms2 pozivom metode ms1.Equals te ispisati rezultat
+            Console.WriteLine(ms1.Equals(ms2));
 
-            // TODO: Usporediti objekte ms1 i ms2 pozivom metode ms1.Equals te ispisati rezultat
-
-
-            // TODO: Usporediti objekte ms1 i ms2 pozivom metode ms2.Equals te ispisati rezultat
-
+            // Usporediti objekte ms1 i ms2 pozivom metode ms2.Equals te ispisati rezultat
+            Console.WriteLine(ms2.Equals(ms1));
         }
 
         static void Main(string[] args)
--- ToString/ToString.cs
+++ ToString/ToString.cs
@@ -17,8 +17,12 @@ namespace Vsite.CSharp
         public double Realni;
         public double Imaginarni;
 
-        // TODO: Pregaziti (override) metodu ToString tako da vraća niz u obliku: "2+3j", "2-j", "0", "j", "-j".
+        //  Pregaziti (override) metodu ToString tako da vraća niz u obliku: "2+3j", "2-j", "0", "j", "-j".
+        public override string ToString()
+        {
 
+            return string.Format("{0}+{1}j",Realni,Imaginarni);
+        }
 
     }
 
